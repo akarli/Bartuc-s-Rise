7d8c9d87897e95fe588f63c04ade4c86c37d8409
akarli <karlin.andreas@gmail.com>
2014-04-27 15:07:11 +0200

    Update Room.java

--- Room.java
+++ Room.java
@@ -7,13 +7,20 @@ import javax.imageio.ImageIO;
 
 public class Room {
 	private int[][] map;
+	private int[][] map2;
 	private BufferedImage tileSheet;
 
-	public Room(int[][] existingMap) {
+	public Room(int[][] existingMap, int[][] existingMap2) {
 		map = new int[existingMap.length][existingMap[0].length];
 		for (int y = 0; y < map.length; y++) {
 			for (int x = 0; x < map[y].length; x++) {
 				map[y][x] = existingMap[y][x];
+			}
+		}
+		map2 = new int[existingMap2.length][existingMap2[0].length];
+		for (int y = 0; y < map2.length; y++) {
+			for (int x = 0; x < map2[y].length; x++) {
+				map2[y][x] = existingMap2[y][x];
 			}
 		}
 		tileSheet = LoadTileSheet("tileset.png");
@@ -37,6 +44,28 @@ public class Room {
 		   for(int y = 0; y < map.length; y++){
 			   for(int x = 0; x < map[y].length; x++){
 				   int index = map[y][x];
+				   int yOffset = 0;
+				   while(index > (tileSheet.getWidth() / Engine.TILE_WIDTH) - 1){
+					   index = index - (tileSheet.getWidth() / Engine.TILE_WIDTH);
+					   yOffset++; 
+				   }
+				   
+				   g.drawImage(tileSheet,
+						   x * Engine.TILE_WIDTH,
+						   y * Engine.TILE_HEIGHT,
+						   (x * Engine.TILE_WIDTH) + Engine.TILE_WIDTH,
+						   (y * Engine.TILE_HEIGHT) + Engine.TILE_HEIGHT,
+						   index * Engine.TILE_WIDTH,
+						   yOffset * Engine.TILE_HEIGHT,
+						   (index * Engine.TILE_WIDTH) + Engine.TILE_WIDTH,
+						   (yOffset * Engine.TILE_HEIGHT) + Engine.TILE_HEIGHT,
+						   null);
+			   }
+		   }
+		   
+		   for(int y = 0; y < map2.length; y++){
+			   for(int x = 0; x < map2[y].length; x++){
+				   int index = map2[y][x];
 				   int yOffset = 0;
 				   while(index > (tileSheet.getWidth() / Engine.TILE_WIDTH) - 1){
 					   index = index - (tileSheet.getWidth() / Engine.TILE_WIDTH);
