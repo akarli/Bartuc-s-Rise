06586bd16eb4f459a5f01976147661fb8220a5e7
wohman <uchtebxd@gmail.com>
2014-04-27 15:39:05 +0200

    Update DrawGame.java

--- DrawGame.java
+++ DrawGame.java
@@ -11,20 +11,15 @@ import javax.swing.JPanel;
 public class DrawGame extends JPanel implements KeyListener, MouseListener, MouseMotionListener{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	Room centralZone, northZone, southZone, eastZone, westZone, caveZone;
+	
 	Character character;
 
 	public DrawGame(){
-		centralZone = new Room(Engine.centralZoneGround, Engine.centralZoneTop);
-		northZone = new Room(Engine.northZoneGround, Engine.northZoneTop);
-		southZone = new Room(Engine.southZoneGround, Engine.southZoneTop);
-		eastZone = new Room(Engine.eastZoneGround, Engine.eastZoneTop);
-		westZone = new Room(Engine.westZoneGround, Engine.westZoneTop);
-		caveZone = new Room(Engine.caveZoneGround, Engine.caveZoneTop);
+
 		character = new Character();
 
 		addMouseListener(this);
@@ -35,7 +30,7 @@ public class DrawGame extends JPanel implements KeyListener, MouseListener, Mous
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		centralZone.drawImage(g);
+		character.getCurrentRoom().drawImage(g);
 		character.drawImage(g);
 	}
 
@@ -85,20 +80,20 @@ public class DrawGame extends JPanel implements KeyListener, MouseListener, Mous
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-	    	switch( keyCode ) { 
-	        	case KeyEvent.VK_W:
-	        		character.moveUp();
-	        		break;
-			case KeyEvent.VK_A:
-	        		character.moveLeft();
-	        		break;
-	        	case KeyEvent.VK_S:
-	        		character.moveDown();
-	        	 	break;
-		 	case KeyEvent.VK_D:
-	        		character.moveRight();
-	        		break;
-	     }
+		switch( keyCode ) {
+		case KeyEvent.VK_W:
+			character.moveUp();
+			break;
+		case KeyEvent.VK_A:
+			character.moveLeft();
+			break;
+		case KeyEvent.VK_S:
+			character.moveDown();
+			break;
+		case KeyEvent.VK_D:
+			character.moveRight();
+			break;
+		}
 	}
 
 	@Override
