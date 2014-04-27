7b67efb9a9d923df7f8cadb0472e0dedabc35e45
wohman <uchtebxd@gmail.com>
2014-04-27 22:04:46 +0200

    Update Character.java

--- Character.java
+++ Character.java
@@ -13,12 +13,12 @@ public class Character {
 
 
 	public Character(){
-		centralZone = new Room(Engine.centralZoneGround, Engine.centralZoneTop);
-		northZone = new Room(Engine.northZoneGround, Engine.northZoneTop);
-		southZone = new Room(Engine.southZoneGround, Engine.southZoneTop);
-		eastZone = new Room(Engine.eastZoneGround, Engine.eastZoneTop);
-		westZone = new Room(Engine.westZoneGround, Engine.westZoneTop);
-		caveZone = new Room(Engine.caveZoneGround, Engine.caveZoneTop);
+		centralZone = new Room(Engine.centralZoneGround, Engine.centralZoneTop, Engine.centralZoneCollision);
+		northZone = new Room(Engine.northZoneGround, Engine.northZoneTop, Engine.northZoneCollision);
+		southZone = new Room(Engine.southZoneGround, Engine.southZoneTop, Engine.southZoneCollision);
+		eastZone = new Room(Engine.eastZoneGround, Engine.eastZoneTop, Engine.eastZoneCollision);
+		westZone = new Room(Engine.westZoneGround, Engine.westZoneTop, Engine.westZoneCollision);
+		caveZone = new Room(Engine.caveZoneGround, Engine.caveZoneTop, Engine.caveZoneCollision);
 		centralZone.setExit("north", northZone);
 		centralZone.setExit("west", westZone);
 		centralZone.setExit("south", southZone);
@@ -37,69 +37,78 @@ public class Character {
 	}
 	public BufferedImage loadCharacterImage(String fileName){
 		BufferedImage img = null;
-		
+
 		try{ 
 			img = ImageIO.read(new File(fileName));
 		} catch (IOException e1) {
 			throw new IllegalArgumentException("Could not load the file.");
 		}
-		
+
 		return img;
 	}
-	
+
 	public int getX(){
 		return xPosition;
 	}
-	
+
 	public int getY(){
 		return yPosition;
 	}
-	
+
 	public void moveUp(){
-		yPosition -= 32;
-		if(yPosition < 0){
+		if(yPosition == 0){
 			currentRoom = currentRoom.getExit("north");
-			yPosition = 618;
+			yPosition = 608;
+		}
+		else if(currentRoom.getCollisionMap()[((yPosition-31)/32)][((xPosition+1)/32)] != 1){
+			yPosition -= 32;
 		}
 	}
 
 	public void moveDown(){
-		yPosition +=32;
-		if(yPosition > 640){
+		if(yPosition == 608){
 			currentRoom = currentRoom.getExit("south");
 			yPosition = 0;
 		}
+		else if(currentRoom.getCollisionMap()[((yPosition+33)/32)][((xPosition+1)/32)] != 1){
+			yPosition +=32;
+
+		}
 	}
-	
+
 	public void moveLeft(){
-		xPosition-= 32;
-		if(xPosition < 0){
+		if(xPosition == 0){
 			currentRoom = currentRoom.getExit("west");
 			xPosition = 992;
 		}
+		else if(currentRoom.getCollisionMap()[((yPosition+1)/32)][((xPosition-31)/32)] != 1){
+			xPosition-= 32;
+		}
 	}
-	
+
 	public void moveRight(){
-		xPosition += 32;
-		if(xPosition > 992){
+		if(xPosition == 992){
 			currentRoom = currentRoom.getExit("east");
 			xPosition = 0;
 		}
+		else if(currentRoom.getCollisionMap()[((yPosition+1)/32)][((xPosition+33)/32)] != 1){
+			xPosition += 32;
+		}
 	}
-	
+
 	public void setX(int x){
 		xPosition = x;
 	}
-	
+
 	public void setY(int y){
 		yPosition = y;
 	}
-	
+
 	public Room getCurrentRoom(){
 		return currentRoom;
 	}
-	
-	
+
+
 	public void drawImage(Graphics g){
 		g.drawImage(character, xPosition, yPosition, null);
 	}
