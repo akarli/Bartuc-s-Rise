caba36ebed5ea6fa46c45fbabb6c48d45b35a4ea
akarli <karlin.andreas@gmail.com>
2014-05-05 15:51:32 +0200

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -11,26 +11,53 @@ public class GameMain extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	static GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	static GraphicsDevice gs = ge.getDefaultScreenDevice();
-	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	JPanel gamePanel = new JPanel(); // Panel containing the game
+	JPanel infoPanel = new JPanel(); // Panel containing the stats and info boxes
 	
-	DrawGame draw = new DrawGame();
+	DrawGame draw = new DrawGame(); // The game
 	
 	Timer t = new Timer(1000/60, this);
 	
 	public GameMain(){
-		super("Bartuc's Rise");
+		super("Bartuc's Rise"); // Names the window
+		t.start(); // Starts the game timer
 		
-		t.start();
+		/**
+		 * GAME WINDOW PREFERENCES
+		 */
 		
-		setPreferredSize(new Dimension(32*Engine.TILE_WIDTH, 20*Engine.TILE_HEIGHT+25));
+		setLayout(null); // Allows for free placement of components in this window
+		setPreferredSize(new Dimension(32*Engine.TILE_WIDTH, 20*Engine.TILE_HEIGHT+225)); // Sets the size for the game window
 		setResizable(false);
 		setUndecorated(false);
 		setBackground(Color.WHITE);  
 		//gs.setFullScreenWindow(this);
 		
-		getContentPane().add(draw);
-		pack();
+		/**
+		 * PANEL PREFERENCES
+		 */
 		
+		gamePanel.setBounds(0, 0, 1024, 640); // Sets the size and position of the game panel
+		infoPanel.setBounds(0, 640, 1024, 200); // Sets the size and position for the info panel
+		infoPanel.setBackground(Color.WHITE);
+		infoPanel.setLayout(null); // Allows for free placement of components in this panel
+
+		/**
+		 * GAME PREFERENCES
+		 */
+		
+		draw.setPreferredSize(new Dimension(1024, 640)); // Sets the size of the game
+		
+		/**
+		 * BUILDNING THE WINDOW
+		 */
+		
+		getContentPane().add(gamePanel); // Adds the game panel to its position
+		getContentPane().add(infoPanel); // Adds the info pnale to its position
+		gamePanel.add(draw); // Adds the game to the game panel
+		
+		pack();
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		}
