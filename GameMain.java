3fd9bbd15a2d7973496cc8df093716484d8d24e7
akarli <karlin.andreas@gmail.com>
2014-05-08 17:40:56 +0200

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -278,7 +278,7 @@ public class GameMain extends JFrame implements ActionListener {
 		    
 		    infoBox.append(Engine.saveMessage);
 		} catch (IOException ex) {
-		  infoBox.append("\n Something went wrong, game not saved");
+		  infoBox.append("\n Something went wrong, game not saved.");
 		  return;
 		} finally {
 		   writer.close();
