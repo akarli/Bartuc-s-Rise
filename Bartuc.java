28dc5f5d73a387e045e42176d40aa3f1a529bbad
wohman <uchtebxd@gmail.com>
2014-05-14 21:21:59 +0200

    Update Bartuc.java

--- Bartuc.java
+++ Bartuc.java
@@ -11,16 +11,20 @@ import javax.imageio.ImageIO;
 public class Bartuc {
 
 	private int maxHealth, currentHealth, baseHealth;
-	private int damage, baseDamage, attackCooldown, difficulty, xPosition, yPosition, shadowBlastCounter;
-	private BufferedImage bartuc, currentSprite;
+	private int damage, baseDamage, attackCooldown, difficulty, xPosition, yPosition, shadowBlastCounter, sprite, deathCounter, firesprite;
+	private BufferedImage bartuc, currentSprite , superfireblast;
 	private BufferedImage[] bartucIdle;
+	private BufferedImage[] superfireblasts;
 	private ShadowBlast shadowBlasts[];
 	private Room zone;
 	private boolean casting = false;
 	public boolean aggro = false;
 	public boolean alive = true;
+	public boolean dying;
 	Random rand = new Random();
 	private MediaPlayer hitPlayer;
+	private MediaPlayer deathPlayer;
+	private MediaPlayer deathThemePlayer;
 	private boolean hitSound = false;
 
 	public Bartuc(int kills){
@@ -35,7 +39,12 @@ public class Bartuc {
 		attackCooldown = 10;
 		shadowBlasts = new ShadowBlast[10];
 		bartuc = loadBartucImage("Graphics\\bartuc.png");
+		superfireblast = loadBartucImage("Graphics\\superfireball.png");
 		bartucIdle = new BufferedImage[3];
+		superfireblasts = new BufferedImage[42];
+		for(int i = 0; i<superfireblasts.length; i++){
+			superfireblasts[i]= superfireblast.getSubimage(i*256, 0, 256, 256);
+		}
 		bartucIdle[0] = bartuc.getSubimage(0, 0, 64, 50);
 		bartucIdle[1] = bartuc.getSubimage(67, 0, 64, 50);
 		bartucIdle[2] = bartuc.getSubimage(134, 0, 65, 50);
@@ -43,6 +52,10 @@ public class Bartuc {
 		zone = Engine.caveZone;
 		hitPlayer = new MediaPlayer(Engine.bartucHit1);
 		hitPlayer.setVolume(0.2f);
+		deathPlayer = new MediaPlayer(Engine.bartucDeath);
+		deathPlayer.setVolume(0.5f);
+		deathThemePlayer = new MediaPlayer(Engine.bartucDeathTheme);
+		deathThemePlayer.setVolume(0.2f);
 		for(int i = 0; i<10; i++){
 			shadowBlasts[i] = new ShadowBlast();
 		}
@@ -62,11 +75,32 @@ public class Bartuc {
 
 
 	public void drawImage(Graphics g){
-		g.drawImage(bartucIdle[1], xPosition, yPosition, null);
+		if(dying){
+			g.drawImage(superfireblasts[firesprite/3], xPosition-100, yPosition-100, null);
+			if(deathCounter < 100){
+				g.drawImage(bartucIdle[sprite/5], rand.nextInt(10) + 476, rand.nextInt(10) + 123, null);
+			}
+			deathCounter++;
+			sprite++;
+			firesprite++;
+			if(sprite>= 15){
+				sprite = 0;
+			}
+			if(firesprite >= 126){
+				firesprite = 0;
+			}
+		}
+		else{
+			g.drawImage(bartucIdle[1], xPosition, yPosition, null);
+		}
 		if(casting){
 			for(int i = 0; i< shadowBlasts.length; i++){
 				shadowBlasts[i].drawImage(g);
 			}
+		}
+		if(deathCounter == 126){
+			die();
+			deathCounter = 0;
 		}
 	}
 
@@ -86,15 +120,15 @@ public class Bartuc {
 	public int getCD(){
 		return attackCooldown;
 	}
-	
+
 	public int getXTile(){
-		  return xPosition/32;
-		 }
-		 
-		 public int getYTile(){
-		  return yPosition/32;
+		return xPosition/32;
 	}
-		 
+
+	public int getYTile(){
+		return yPosition/32;
+	}
+
 	public void takeDamage(int damage){
 		currentHealth -= damage;
 		int sound = rand.nextInt(2);
@@ -114,12 +148,15 @@ public class Bartuc {
 		}
 		hitSound = false;
 	}
-	
+
 	public void reset(){
+		xPosition = 480;
+		yPosition = 128;
 		aggro = false;
+		dying = false;
 		Engine.bartucThemeSound = false;
 	}
-	
+
 	public void die(){
 		DrawGame.character.addBartucKills();
 		reset();
@@ -131,7 +168,15 @@ public class Bartuc {
 		GameMain.infoBox.append(Engine.bartucDeathMessage);
 		GameMain.infoBox.setCaretPosition(GameMain.infoBox.getDocument().getLength());
 	}
-	
+
+	public void startDying(){
+		if(!dying){
+			new Thread(deathThemePlayer).start();
+			new Thread(deathPlayer).start();
+		}
+		dying = true;
+	}
+
 	public void spawn(int kills){
 		alive = true;
 		aggro = false;
@@ -151,10 +196,10 @@ public class Bartuc {
 			shadowBlastCounter = 0;
 		}
 	}
-	
+
 	public void stopCasting() {
 		for (int i = 0; i < shadowBlasts.length; i++) {
 			shadowBlasts[i].stop();
 		}
- }
+	}
 }
